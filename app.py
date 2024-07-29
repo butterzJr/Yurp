2608dca3265903cac02c1d717a8d2754e02a2cd8
Lisayurp <elisafranks11@gmail.com>
2024-07-29 12:53:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 import streamlit as st
 
-# Set the title of the app
-st.title("welcome to my first Streamlit App")
+st.title("Welcome to my first Streamlit App")
+name = st.text_input("Enter your name:")
 
-# Add a text input
-name = st.text_input("Enter your name: ")
-
-# Display the name entered by the user
 if name:
     st.write(f"Hello, {name}! Welcome to the app.")
